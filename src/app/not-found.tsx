fa8590be83156d8ed6c1332432fcb7adee7fefe4
morteza <mori2002sa@gmail.com>
2024-07-31 02:01:39 +0330

    Improve not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,28 @@
+import { Box, Button } from "@mui/material";
+import { useLocale, useTranslations } from "next-intl";
+import Link from "next/link";
+
 const NotFound = () => {
+  const t = useTranslations("NotFound");
+  const currentLocale = useLocale();
+
   return (
     <html>
       <body>
-        <h1>Something Went Wrong</h1>
+        <Box
+          sx={{
+            display: "flex",
+            height: "100vh",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <h1>{t("error_message")}</h1>
+          <Link href={`/${currentLocale}/sign-in`}>
+            <Button variant={"contained"}>{t("button_label")}</Button>
+          </Link>
+        </Box>
       </body>
     </html>
   );
